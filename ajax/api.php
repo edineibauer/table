6687c8163559ff420a7008288ffcf736373195d7
Edinei <galera.org@gmail.com>
2018-07-25 11:30:47 -0300

    list mult ownerPublisher

--- ajax/api.php
+++ ajax/api.php
@@ -3,5 +3,5 @@ $entity = strip_tags(trim(filter_input(INPUT_POST, 'entity', FILTER_DEFAULT)));
 
 if ($entity && !empty($entity)) {
     $table = new \Table\Table($entity);
-    $data['data'] = $table->getShow();
+    $data = $table->getShow();
 }
