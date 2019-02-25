3761286e5afaf2cb90b3718c7b903cfffabadea0
Edinei J. Bauer <galera.org@gmail.com>
2019-02-25 17:30:41 -0300

    update status old data

--- public/ajax/update/status.php
+++ public/ajax/update/status.php
@@ -4,11 +4,14 @@ $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 $status = strip_tags(trim(filter_input(INPUT_POST, 'status', FILTER_VALIDATE_BOOLEAN)));
 $entity = strip_tags(trim(filter_input(INPUT_POST, 'entity', FILTER_DEFAULT)));
 $col = strip_tags(trim(filter_input(INPUT_POST, 'col', FILTER_DEFAULT)));
-$dados = [$col => ($status ? 1 : 0), "id" => $id];
+$dados = [$col => $status === "1" ? 1 : 0, "id" => $id];
+
+$read = new \ConnCrud\Read();
+$read->exeRead($entity, "WHERE id = :id", "id={$id}");
+if($read->getResult()) {
+    $oldDados = $read->getResult()[0];
+}
 
 $up = new \ConnCrud\Update();
 $up->exeUpdate($entity, $dados, "WHERE id = :id", "id={$id}");
-
-$dd = new \Entity\Dicionario($entity);
-$oldDados = $dd->getDataForm();
 new \Entity\React("update", $entity, $dados, $oldDados);
